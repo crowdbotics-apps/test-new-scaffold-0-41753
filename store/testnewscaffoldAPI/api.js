fd043c4b6f88d7361c40fdc82ba3c8322dadff75
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-06-27 16:23:23 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/testnewscaffoldAPI/api.js
+++ store/testnewscaffoldAPI/api.js
@@ -1,67 +1,84 @@
-import axios from "axios"
+import axios from "axios";
 const testnewscaffoldAPI = axios.create({
   baseURL: "https://test-new-scaffold-0-41753.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return testnewscaffoldAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return testnewscaffoldAPI.post(`/api/v1/login/`, payload.data)
+  return testnewscaffoldAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return testnewscaffoldAPI.post(`/api/v1/signup/`, payload.data)
+  return testnewscaffoldAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function modules_camera_photos_user_list(payload) {
-  return testnewscaffoldAPI.get(`/modules/camera/photos/user/`)
+  return testnewscaffoldAPI.get(`/modules/camera/photos/user/`);
 }
+
 function modules_camera_photos_user_retrieve(payload) {
-  return testnewscaffoldAPI.get(`/modules/camera/photos/user/${payload.id}/`)
+  return testnewscaffoldAPI.get(`/modules/camera/photos/user/${payload.id}/`);
 }
+
 function modules_camera_upload_image_create(payload) {
-  return testnewscaffoldAPI.post(`/modules/camera/upload_image/`)
+  return testnewscaffoldAPI.post(`/modules/camera/upload_image/`);
 }
+
 function rest_auth_login_create(payload) {
-  return testnewscaffoldAPI.post(`/rest-auth/login/`, payload.data)
+  return testnewscaffoldAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return testnewscaffoldAPI.get(`/rest-auth/logout/`)
+  return testnewscaffoldAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return testnewscaffoldAPI.post(`/rest-auth/logout/`)
+  return testnewscaffoldAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return testnewscaffoldAPI.post(`/rest-auth/password/change/`, payload.data)
+  return testnewscaffoldAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return testnewscaffoldAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return testnewscaffoldAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return testnewscaffoldAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return testnewscaffoldAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return testnewscaffoldAPI.post(`/rest-auth/registration/`, payload.data)
+  return testnewscaffoldAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return testnewscaffoldAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return testnewscaffoldAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return testnewscaffoldAPI.get(`/rest-auth/user/`)
+  return testnewscaffoldAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return testnewscaffoldAPI.put(`/rest-auth/user/`, payload.data)
+  return testnewscaffoldAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return testnewscaffoldAPI.patch(`/rest-auth/user/`, payload.data)
+  return testnewscaffoldAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -80,4 +97,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
