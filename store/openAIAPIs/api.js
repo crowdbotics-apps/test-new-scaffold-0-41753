dcfd1868ff39122d212b822a4fe419ab371e9e1f
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-07-03 16:58:18 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/openAIAPIs/api.js
+++ store/openAIAPIs/api.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-import { OPENAI_TOKEN } from "react-native-dotenv"
+import axios from "axios";
+import { OPENAI_TOKEN } from "react-native-dotenv";
 const openAIAPIs = axios.create({
   baseURL: "https://api.openai.com",
   headers: {
@@ -7,31 +7,40 @@ const openAIAPIs = axios.create({
     "Content-Type": "application/json",
     Authorization: `Bearer ${OPENAI_TOKEN}`
   }
-})
+});
+
 function openaiapis_post_v1_edits_create(payload) {
-  return openAIAPIs.post(`/v1/edits`, payload.data)
+  return openAIAPIs.post(`/v1/edits`, payload.data);
 }
+
 function openaiapis_get_v1_models_list(payload) {
-  return openAIAPIs.get(`/v1/models`)
+  return openAIAPIs.get(`/v1/models`);
 }
+
 function openaiapis_get_v1_engines_list(payload) {
-  return openAIAPIs.get(`/v1/engines`)
+  return openAIAPIs.get(`/v1/engines`);
 }
+
 function openaiapis_post_v1_completions_create(payload) {
-  return openAIAPIs.post(`/v1/completions`, payload.data)
+  return openAIAPIs.post(`/v1/completions`, payload.data);
 }
+
 function openaiapis_get_v1_models_model_read(payload) {
-  return openAIAPIs.get(`/v1/models/${payload.model}`)
+  return openAIAPIs.get(`/v1/models/${payload.model}`);
 }
+
 function openaiapis_post_v1_chat_completions_create(payload) {
-  return openAIAPIs.post(`/v1/chat/completions`, payload.data)
+  return openAIAPIs.post(`/v1/chat/completions`, payload.data);
 }
+
 function openaiapis_post_v1_images_generations_create(payload) {
-  return openAIAPIs.post(`/v1/images/generations`, payload.data)
+  return openAIAPIs.post(`/v1/images/generations`, payload.data);
 }
+
 function openaiapis_get_v1_engines_engine_id_read(payload) {
-  return openAIAPIs.get(`/v1/engines/${payload.engine_id}`)
+  return openAIAPIs.get(`/v1/engines/${payload.engine_id}`);
 }
+
 export const apiService = {
   openaiapis_post_v1_edits_create,
   openaiapis_get_v1_models_list,
@@ -41,4 +50,4 @@ export const apiService = {
   openaiapis_post_v1_chat_completions_create,
   openaiapis_post_v1_images_generations_create,
   openaiapis_get_v1_engines_engine_id_read
-}
+};
