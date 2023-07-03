f7e8c2b53c2878770a485187f06ca0e3b4fccba1
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-07-03 18:55:09 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/openAIAPIs/openaiapis_response_get_Listengines.slice.js
+++ store/openAIAPIs/openaiapis_response_get_Listengines.slice.js
@@ -1,13 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const openaiapis_get_v1_engines_list = createAsyncThunk(
-  "openaiapis_response_get_Listengines/openaiapis_get_v1_engines_list",
-  async payload => {
-    const response = await apiService.openaiapis_get_v1_engines_list(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const openaiapis_get_v1_engines_list = createAsyncThunk("openaiapis_response_get_Listengines/openaiapis_get_v1_engines_list", async payload => {
+  const response = await apiService.openaiapis_get_v1_engines_list(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const openaiapis_response_get_ListenginesSlice = createSlice({
   name: "openaiapis_response_get_Listengines",
   initialState,
@@ -15,24 +18,24 @@ const openaiapis_response_get_ListenginesSlice = createSlice({
   extraReducers: {
     [openaiapis_get_v1_engines_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [openaiapis_get_v1_engines_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
+        state.entities = action.payload;
+        state.api.loading = "idle";
       }
     },
     [openaiapis_get_v1_engines_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   openaiapis_get_v1_engines_list,
   slice: openaiapis_response_get_ListenginesSlice
-}
+};
--- store/openAIAPIs/openaiapis_response_get_Listmodels.slice.js
+++ store/openAIAPIs/openaiapis_response_get_Listmodels.slice.js
@@ -1,13 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const openaiapis_get_v1_models_list = createAsyncThunk(
-  "openaiapis_response_get_Listmodels/openaiapis_get_v1_models_list",
-  async payload => {
-    const response = await apiService.openaiapis_get_v1_models_list(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const openaiapis_get_v1_models_list = createAsyncThunk("openaiapis_response_get_Listmodels/openaiapis_get_v1_models_list", async payload => {
+  const response = await apiService.openaiapis_get_v1_models_list(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const openaiapis_response_get_ListmodelsSlice = createSlice({
   name: "openaiapis_response_get_Listmodels",
   initialState,
@@ -15,24 +18,24 @@ const openaiapis_response_get_ListmodelsSlice = createSlice({
   extraReducers: {
     [openaiapis_get_v1_models_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [openaiapis_get_v1_models_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
+        state.entities = action.payload;
+        state.api.loading = "idle";
       }
     },
     [openaiapis_get_v1_models_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   openaiapis_get_v1_models_list,
   slice: openaiapis_response_get_ListmodelsSlice
-}
+};
